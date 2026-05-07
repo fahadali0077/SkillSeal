aa2b0b2e61ba55c644157d4fd1a42b6480967f04
fahadali0077 <fahaj698@gmail.com>
2026-05-07 12:00:41 +0500

    v6

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import { createServer } from 'http';
 import { initSocket } from './config/socket';
 import logger from './utils/logger';
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 async function bootstrap() {
   try {
